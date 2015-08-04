1949c0d434ccf48aadc294ddac2d630fc7a1fbf4
Josue Valverde & Yelena Nedelko <student@epicodus.com>
2015-08-04 14:04:30 -0700

    Finished scrabble program by adding spec where method returns points for a specific word.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -2,17 +2,13 @@ class String
   define_method(:scrabble) do
     scrabble_points = {"aeioulnrst" => 1, "dg" => 2, "bcmp" => 3, "fhvwy" => 4, "k" => 5, "jx" => 8, "qz" => 10}
     points = 0
-    scrabble_points.each() do |scrabble_key_value|
-      if scrabble_key_value[0].include?(self)
-        points = points + scrabble_key_value[1]
+    split("").each() do |character|
+      scrabble_points.each() do |scrabble_key_value|
+        if scrabble_key_value[0].include?(character)
+          points = points + scrabble_key_value[1]
+        end
       end
     end
     points
   end
 end
-
-
-#split_letters = scrabble_letters[0].split("")
-
-#if split_letters.include?(self)
-#  points = points + scrabble_points.fetch(scrabble_letters[0])
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,4 +5,8 @@ describe('String#scrabble') do
   it("returns a scrabble score for a letter") do
     expect(("f").scrabble()).to(eq(4))
   end
+
+  it("returns a scrabble score for a word") do
+    expect(("happy").scrabble()).to(eq(15))
+  end
 end
